ecec405ea5ccb614c55d36b82e793cc2fe58b6d3
shuimo1 <guan1502351@163.com>
2017-07-20 13:57:41 +0800

    查询方法

--- src/main/java/com/qianfeng/dao/UsersDao.java
+++ src/main/java/com/qianfeng/dao/UsersDao.java
@@ -5,5 +5,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersDao {
+    //只查询单个用户
     void login(Users users);
+    //查询需求的用户,用于模糊查询
+    void findUsers(String str);
+    //查询所有用户，用于管理员操作
+    void findAllUsers();
 }
--- src/main/java/com/qianfeng/service/UsersServices.java
+++ src/main/java/com/qianfeng/service/UsersServices.java
@@ -1,7 +1,16 @@
 package com.qianfeng.service;
 
+import com.qianfeng.dao.UsersDao;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class UsersServices {
+    @Resource
+    private UsersDao ud;
+    //多单个用户查询服务
+    public void login(){
+
+    }
 }
