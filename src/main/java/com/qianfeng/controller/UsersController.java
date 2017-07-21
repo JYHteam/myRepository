51f26add9552941d64749ce5d9cfa575ed64e290
8fire <459816669@qq.com>
2017-07-21 09:45:21 +0800

    jin 导航栏搭建

--- src/main/java/com/qianfeng/controller/UsersController.java
+++ src/main/java/com/qianfeng/controller/UsersController.java
@@ -1,15 +1,23 @@
 package com.qianfeng.controller;
 
+import com.qianfeng.bean.Users;
+import com.qianfeng.dao.UsersDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Controller
 public class UsersController {
+    @Resource
+    private UsersDao ud;
     @RequestMapping("dologin.do")
     @ResponseBody
-    public String doLogin(){
-        return "success";
+    public List<Users> doLogin(){
+
+        return ud.findAllUsers();
     }
     @ResponseBody
     public String Login11(){
--- src/main/java/com/qianfeng/dao/UsersDao.java
+++ src/main/java/com/qianfeng/dao/UsersDao.java
@@ -3,6 +3,8 @@ package com.qianfeng.dao;
 import com.qianfeng.bean.Users;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsersDao {
     //只查询单个用户
@@ -10,5 +12,5 @@ public interface UsersDao {
     //查询需求的用户,用于模糊查询
     void findUsers(String str);
     //查询所有用户，用于管理员操作
-    void findAllUsers();
+    List<Users> findAllUsers();
 }
